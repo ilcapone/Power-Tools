3eb9a6b5bd894ae97d140e775f4f17f2ea2f1b94
ilcapone <juanzumbido@hotmail.com>
2019-04-07 11:24:17 +0200

    add base 64 code and decode

---
+++ base64/convertToBase64.ps1
@@ -0,0 +1,21 @@
+﻿function convertToBase64
+{
+    param
+    (
+        [string] $SourceFilePath,
+        [string] $TargetFilePath
+    )
+    $bufferSize = 9000 # debe ser mmltiplo de 4
+    $buffer = New-Object byte[] $bufferSize
+    $reader = [System.IO.File]::OpenRead($SourceFilePath)
+    $writer = [System.IO.File]::CreateText($TargetFilePath)
+    $bytesRead = 0
+    do
+    {
+        $bytesRead = $reader.Read($buffer, 0, $bufferSize);
+        $writer.Write([Convert]::ToBase64String($buffer, 0, $bytesRead));
+
+    } while ($bytesRead -eq $bufferSize)
+    $reader.Dispose()
+    $writer.Dispose()   
+}
