b15823aa3f22ac61f3a9733839d285dbe16a8052
Juan Gordo <jgordo@a2secure.com>
2019-05-03 11:00:40 +0200

    change escritorio

---
+++ Set-WallPaper.ps1
@@ -0,0 +1,7 @@
+﻿Function Set-WallPaper($value)
+{
+ Set-ItemProperty -path 'HKCU:\Control Panel\Desktop\' -name wallpaper -value $value
+ rundll32.exe user32.dll, UpdatePerUserSystemParameters
+}
+curl [Remote url image] -OutFile C:\Users\Public\judia.jpg
+Set-WallPaper -value C:\Users\Public\judia.jpg
